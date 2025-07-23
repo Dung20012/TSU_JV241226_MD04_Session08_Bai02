311c9df0badcd7c78aa3999f5a45fdf61b1256dc
Dung20012 <hantho1107@gmail.com>
2025-07-23 11:17:00 +0700

    Add files via upload

---
+++ session_08/model/entity/Category.java
@@ -0,0 +1,27 @@
+package com.data.session_08.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Table(name = "categories")
+public class Category {
+    @Id
+    @Column(name = "CATEGORY_ID")
+    private Long id;
+    private String cateName;
+    private String description;
+    private Boolean status;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Book> books;
+}
